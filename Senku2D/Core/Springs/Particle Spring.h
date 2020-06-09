01f4c6337af5e511c87e5b64885e9db5ef12b05a
Arsh Panesar <aparsh123@gmail.com>
2020-06-09 14:32:01 +0530

    Added a Basic Spring Generator

---
+++ Senku2D/Core/Springs/Particle Spring.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "../Particle Force Generator.h"
+//Defines
+#define Real_Abs fabsf
+
+namespace Senku2D
+{
+	//A Basic Spring Force Generator
+	/*
+	This Basic Spring Force Generator Works by Considering Two Particles Joined
+	at the End Together by the Spring
+	*/
+	class ParticleSpring : public IParticleForceGenerator
+	{
+	private:
+		//Particle at the Other End of the Spring
+		Particle* p_OtherParticle;
+
+		//Spring Constant
+		Real m_SpringConstant;
+
+		//Resting Length of the Spring
+		Real m_RestLength;
+
+	public:
+		//Construct a Particle Spring Generator
+		ParticleSpring(Particle* _Other, const Real& _SC, const Real& _RL);
+
+		//Update Force Override
+		void UpdateForce(Particle* _Particle, const Real& Timestep) override;
+	};
+}
