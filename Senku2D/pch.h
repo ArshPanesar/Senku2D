7490d9f0fe4ea8f3fdf82cc90448b533808852ec
Arsh Panesar <aparsh123@gmail.com>
2020-07-22 23:57:26 +0530

    Fix #3

--- Senku2D/pch.h
+++ Senku2D/pch.h
@@ -4,40 +4,25 @@
 #include <iostream>
 #include <vector>
 
-//Logging To Console
-template<typename T>
-inline void PRT(const T& x)
+namespace Senku2D
 {
-	std::cout << x << std::endl;
-}
 
-template<typename T1, typename T2>
-inline void PRT2(const T1& x, const T2& y)
-{
-	std::cout << x << "\t" << y << std::endl;
-}
-
-template<typename T>
-inline void PRTERR(const T& x)
-{
-	std::cerr << x << std::endl;
-}
-
-template<typename T>
-inline void PRTERR2(const T& x, const T& y)
-{
-	std::cerr << x << "\t" << y << std::endl;
-}
-
-template<typename T>
-inline void PRTARR(T& arr, const int& size)
-{
-	for (unsigned int i = 0; i < size; ++i)
+	//Logging To Console
+	//
+	template<typename T>
+	inline void PRT(const T& x)
 	{
-		PRT(arr[i], ", ");
+		std::cout << x << std::endl;
 	}
-}
+	//
+	template<typename T1, typename T2>
+	inline void PRT2(const T1& x, const T2& y)
+	{
+		std::cout << x << "\t" << y << std::endl;
+	}
+	//
 
-//Angle Constants
-const float RADTODEG = 57.2958f;
-const float DEGTORAD = 0.01745f;
+	//Angle Constants
+	const float RADTODEG = 57.2958f;
+	const float DEGTORAD = 0.01745f;
+}
